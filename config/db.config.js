b04b0138f8d9521167a4c0e281948e07e1a748a0
Joice-Deo <babitergulingguling00@gmail.com>
2026-06-03 11:36:23 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize";
 import mysql2 from 'mysql2';
-const db = new Sequelize('web_lanjut', 'root', '', {
-    host: "localhost",
+const db = new Sequelize('web_lanjut', 'avnadmin', 'AVNS_b_-o4_FtcjgzqiuL0DA', {
+    host: "mysql-10db1ea7-weblanjut2.h.aivencloud.com",
     dialect: "mysql",
+    port: 17510,
+    dialectOptions: {
+ssl: {
+rejectUnauthorized: false
+ }
+ },
     "define": {
         "timestamps": false
     }
