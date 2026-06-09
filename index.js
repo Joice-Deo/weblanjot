16f467131afed4e89a40cb457b87602e6d2d0cac
Joice-Deo <babitergulingguling00@gmail.com>
2026-06-09 22:19:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { authenticateToken } from "../middleware/VerifyTokens.js";
 import express from "express";
 import db from "./config/db.config.js";
 import bukuRoutes from "./routes/buku.routes.js";
@@ -6,17 +5,35 @@ import prodiRoutes from "./routes/prodi.routes.js";
 import mahasiswaRoutes from "./routes/mahasiswa.routes.js";
 import pinjams from "./routes/pinjams.routes.js";
 import detail_pinjams from "./routes/detail_pinjam.routes.js";
-import cors from "cors";
 import User from "./routes/user.routes.js";
+import { authenticateToken } from "./middleware/VerifyTokens.js";
+import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config ();
+
+dotenv.config();
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/buk", authenticateToken, bukuRoutes);
+app.use("/api/mahasiswa", mahasiswaRoutes);
+app.use("/api/prodi", prodiRoutes);
+app.use("/api/pinjam", pinjams);
+app.use("/api/detail_pinjam", detail_pinjams);
+app.use("/api/user", User);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello coba backend untuk vercel",
+  });
+});
+
 try {
   await db.authenticate();
   console.log("Database connected");
-
-  await db.sync();
-  console.log("Tables created");
 
   app.listen(5000, () => {
     console.log("Server running on port 5000");
@@ -24,17 +41,3 @@ try {
 } catch (error) {
   console.error(error);
 }
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api/buk",authenticateToken, bukuRoutes);
-app.use("/api/mahasiswa", mahasiswaRoutes);
-app.use("/api/prodi", prodiRoutes);
-app.use("/api/pinjam", pinjams);
-app.use("/api/detail_pinjam", detail_pinjams);
-app.use("/api/user", User);
-app.get('/',(req,res)=>{
-res.json({message:"Hello coba backend untuk vercel"});
-});
-app.listen(5000);
-
